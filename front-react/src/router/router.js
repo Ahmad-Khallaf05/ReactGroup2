c115e9daf6423fb0eb124c75e76a7fe44b51de84
Abdallh-hatamleh <abdallh.a.hatamleh@gmail.com>
2024-09-25 23:29:04 +0300

    halfdone with task crud

--- front-react/src/router/router.js
+++ front-react/src/router/router.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Landing from "../components/pages/land";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import User from '../components/admin/users';
+import Tasks from '../components/admin/tasks';
 import Home from "../components/pages/admin";
 
 //  import "../../public/assets/css/style.css"; 
@@ -16,6 +17,7 @@ export default function Myrouter() {
                     <Route path="/" element={<Landing />} />
                     <Route path="/dashboard" element={<Home />} />
                     <Route path="/Users" element={<User />} />
+                    <Route path="/Tasks" element={<Tasks />} />
                 </Routes>
             </Router>
 
